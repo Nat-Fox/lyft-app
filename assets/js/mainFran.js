7b8d50b4b5a74d0871e2793acb8c749902cc37b5
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-07-02 16:06:57 -0400

    funcion que genera LABxxx envia alert + localStorage OK

--- assets/js/mainFran.js
+++ assets/js/mainFran.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    $('#number').text(Math.floor(Math.random() * 899 + 100));
+    var randNum = ("LAB-" + (Math.floor(Math.random() * 899 + 100)));
+    alert("Tu código es (lo deberás ingresar en el siguiente paso)" + "\n" + randNum);
+
+    localStorage["randNum"] = $("#randNum").text();
+
+
+
 });
-//funcion para rellenar con 0s cuando aleatorio sea <=99
-/* function pad(str, max) {
-     str = num.toString();
-     return str.length < max ? pad("0" + str, max) : str;
- }*/
-
-
-//hacer un if numero random menor de 3 digitos entonces va un 0 al inicio

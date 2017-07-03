502ec6e7ab2f33a0faa5980d4e750feccd8aa3c2
Maria Paz <maria.pazrojasc@gmail.com>
2017-07-03 15:37:03 -0400

    update

--- assets/js/mainPaz.js
+++ assets/js/mainPaz.js
@@ -1,51 +1,51 @@
 function initMap() {
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 18,
-          center: {lat: -34.397, lng: 150.644},
-        });
+       var directionsService = new google.maps.DirectionsService;
+       var directionsDisplay = new google.maps.DirectionsRenderer;
+       var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 18,
+         center: {lat: -34.397, lng: 150.644},
+       });
 
-        directionsDisplay.setMap(map);
+       directionsDisplay.setMap(map);
 
-        var input2 = (document.getElementById("destino"));
-        var autocomplete = new google.maps.places.Autocomplete(input2);
-          autocomplete.bindTo("bounds", map);
-        var infoWindow = new google.maps.InfoWindow({map: map});
+       var input2 = (document.getElementById("destino"));
+       var autocomplete = new google.maps.places.Autocomplete(input2);
+         autocomplete.bindTo("bounds", map);
+       var infoWindow = new google.maps.InfoWindow({map: map});
 
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var location = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-      };
-          infoWindow.setPosition(location);
-          infoWindow.setContent("Estas aquí");
-          map.setCenter(location);
-        },
-    function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
+       if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+           var location = {
+             lat: position.coords.latitude,
+             lng: position.coords.longitude
+     };
+         infoWindow.setPosition(location);
+         infoWindow.setContent("Estas aquí");
+         map.setCenter(location);
+       },
+   function() {
+   handleLocationError(true, infoWindow, map.getCenter());
+   });
+ } else {
 
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-        var onChangeHandler = function() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        };
-        document.getElementById('destino').addEventListener('change', onChangeHandler);
-      }
+   handleLocationError(false, infoWindow, map.getCenter());
+ }
+       var onChangeHandler = function() {
+         calculateAndDisplayRoute(directionsService, directionsDisplay);
+       };
+       document.getElementById('destino').addEventListener('change', onChangeHandler);
+     }
 
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        directionsService.route({
-          origin: "Puma 1180, Recoleta, Chile",
-          destination: document.getElementById('destino').value,
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
+     function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+       directionsService.route({
+         origin: "Puma 1180, Recoleta, Chile",
+         destination: document.getElementById('destino').value,
+         travelMode: 'DRIVING'
+       }, function(response, status) {
+         if (status === 'OK') {
+           directionsDisplay.setDirections(response);
+         } else {
+           window.alert('Directions request failed due to ' + status);
+         }
+       });
+     }
